61e719844e414731c8fecce68d59f0a85e048b27
Falk Hoppe <falkhoppe81@gmail.com>
2016-07-22 20:57:06 +0200

    ns

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const config = require('./lib/config')
 const tasks = [
   require('./lib/tasks/image-tasks'),
   require('./lib/tasks/style-tasks'),
+  require('./lib/tasks/font-tasks'),
   require('./lib/tasks/template-tasks'),
   require('./lib/tasks/release-tasks'),
   require('./lib/tasks/development-tasks')
