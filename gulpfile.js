a1f26fa3e334b7a12eb1755f6907379b1e0f3506
Falk Hoppe <falkhoppe81@gmail.com>
2016-07-24 13:42:45 +0200

    deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp')
+const gutil = require('gulp-util')
+const print = require('gulp-print')
 const sequence = require('gulp-sequence')
+const ghPages = require('gulp-gh-pages')
+
 const config = require('./lib/config')
 
 const tasks = [
@@ -16,6 +20,10 @@ tasks.forEach(service => service.load(gulp, config))
 gulp.task('run', sequence(['watch', 'server']))
 
 gulp.task('deploy', ['prod-build'], () => {
-  gulp.src(config.get('distGlob'))
-  .pipe(gulp.dest(config.get('buildPath')))
+  gulp.src(config.get('buildGlob'))
+  .pipe(ghPages({
+    remoteUrl: 'git@github.com:anyhues/anyhues.github.io.git',
+    branch: 'master'
+  }).on('error', gutil.log))
+  .pipe(print())
 })
